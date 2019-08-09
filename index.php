0d3ea0bd24424a2ec338585521a28437aa5821ee
Sheldon Kryger <s.d.kryger@gmail.com>
2019-08-09 02:38:39 -0600

    table style

--- index.php
+++ index.php
@@ -22,8 +22,26 @@
 		
     </head>
     <body>
-        <table class="table table-fluid">
-            <thead>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <!-- Brand -->
+            <a class="navbar-brand" href="index.php">kreegur</a>
+
+            <!-- Toggler/collapsibe Button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="inputValues.php" target="_blank">Input values</a>
+                    </li>
+                </ul>
+            </div>
+        </nav> 
+        <table class="table table-fluid table-striped table-sm m-top-1">
+            <thead class="thead-dark">
                 <tr>
                     <td>
                         Name
@@ -39,7 +57,6 @@
             <tbody id="latestValues">
             </tbody>
         </table>
-        <a href="inputValues.php" target="_blank">Input values</a>
         <script src = "js/jquery-3.3.1.min.js"></script>
         <script src="js/popper.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
